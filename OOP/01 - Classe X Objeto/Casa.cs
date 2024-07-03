888bee40101461d94610230e535f48e4fa74bfe2
devThiagoJorge <thiago.v.jorge@hotmail.com>
2024-07-02 23:10:43 -0300

    Arrumando estrutura

---
+++ OOP/01 - Classe X Objeto/Casa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Classe_X_Objeto
+{
+    public class Casa
+    {
+        public int TamanhoM2 { get; set; }
+        public int Andares { get; set; }
+        public decimal Valor { get; set; }
+        public int NumeroVagas { get; set; }
+    }
+
+    public class Objeto
+    {
+        public Objeto()
+        {
+            var casa = new Casa
+            {
+                Andares = 1,
+                Valor = 1000,
+                NumeroVagas = 2,
+                TamanhoM2 = 15
+            };
+        }
+    }
+}
+
+
---
+++ OOP/02 - Pilares OOP/Heranca.cs
@@ -0,0 +1,24 @@
+﻿
+namespace OOP._02___Pilares_OOP
+{
+    public class Funcionario : Pessoa
+    {
+        public DateTime DataAdmissao { get; set; }
+        public string Registro { get; set; }
+    }
+
+    public class Processo{
+        public void Execucao()
+        {
+            var funcionario = new Funcionario
+            {
+                Nome = "Funcionário 1",
+                DataAdmissao = DateTime.Now,
+                DataNascimento = Convert.ToDateTime("2000/03/04"),
+                Registro = "321421"
+            };
+
+            funcionario.CalcularIdade();
+        }
+    }
+}
