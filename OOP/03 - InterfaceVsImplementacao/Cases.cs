5d419a9449e37af2e80d4ea9808993360b05de2e
devThiagoJorge <thiago.v.jorge@hotmail.com>
2024-07-09 22:32:50 -0300

    Continuação entendendo a diferença entre interface x implementação e herança x composição

---
+++ OOP/03 - InterfaceVsImplementacao/Cases.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP._03___InterfaceVsImplementacao
+{
+    public interface IRepositorio
+    {
+        void Adicionar();
+    }
+
+    public class Repositorio : IRepositorio
+    {
+        public void Adicionar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RepositorioFake : IRepositorio
+    {
+        public void Adicionar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class UsoImplementacao : IRepositorio
+    {
+        public void Adicionar()
+        {
+            var rep = new Repositorio();
+            rep.Adicionar();    
+        }
+    }
+
+    public class UsoAbstracao
+    {
+        private readonly IRepositorio _repositorio;
+
+        public UsoAbstracao(IRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public void Processo()
+        {
+            _repositorio.Adicionar();
+        }
+    }
+
+}
---
+++ OOP/04 - HerancaVsComposicao/Cases.cs
@@ -0,0 +1,43 @@
+﻿using OOP._02___Pilares_OOP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP._04___HerancaVsComposicao
+{
+    public class PessoaFisica : Pessoa
+    {
+        public string Cpf { get; set; }
+    }
+
+    public class PessoaFisica2
+    {
+        public Pessoa Pessoa { get; set; }
+        public string Cpf { get; set; }
+    }
+
+    public class TestesHerancaComposicao
+    {
+        public TestesHerancaComposicao()
+        {
+            var pessoaHeranca = new PessoaFisica
+            {
+                Cpf = "3124141412",
+                DataNascimento = DateTime.Now,
+                Nome = "Herança!"
+            };
+
+            var pessoaComposicao = new PessoaFisica2
+            {
+                Pessoa = new Pessoa
+                {
+                    Nome = "Composição!",
+                    DataNascimento = DateTime.Now,
+                },
+                Cpf = "392183198"
+            };
+        }
+    }
+}
